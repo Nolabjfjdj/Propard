e05e34068547b2eb515cc6a52c1c5942cc2a4f5a
Nolabjfjdj <nolanlabanana@gmail.com>
2026-04-13 19:54:23 +0200

    Clean up comments in Message schema
    Removed comments from the message schema in Message.js.

--- propard-backend/models/Message.js
+++ propard-backend/models/Message.js
@@ -1,28 +1,23 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  // Qui a envoyé le message
   sender: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
 
-  // À qui (conversation entre deux personnes)
   receiver: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
 
-  // Le contenu du message
   content: {
     type: String,
-    required: true,
-    maxlength: 2000
+    required: true
   },
 
-  // Lu ou non
   read: {
     type: Boolean,
     default: false
