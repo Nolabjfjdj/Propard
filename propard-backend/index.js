4275b2543add6fd900f418ce1b589e40cb334417
Propard <propard@outlook.fr>
2026-08-16 04:01:35 +0200

    Fix affichage lien 2

--- propard-backend/index.js
+++ propard-backend/index.js
@@ -141,7 +141,7 @@ io.on('connection', (socket) => {
 // ─── SERVIR LE FRONTEND REACT (BUILD) ──────────────────────────
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
