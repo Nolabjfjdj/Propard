0cf94f423e3c06e4308ba3342422224eef190bbf
Propard <propard@outlook.fr>
2026-09-06 03:22:31 +0200

    Fix flèche page profil iOS 1.

--- propard-frontend/src/pages/AppPage.jsx
+++ propard-frontend/src/pages/AppPage.jsx
@@ -1245,6 +1245,7 @@ export default function AppPage({
 
         {isMobile &&
           (selectedFriend ||
+            selectedProfile ||
             friendNotFound) && (
             <div
               style={
